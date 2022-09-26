c2eea881f8f57cda45ff71c7e1d29c4f820e22d7
Kevin Sasse <sassek70@gmail.com>
2022-09-25 20:16:27 -0400

    commit tree a9c2ec2592f87e3c5cf62a41e66271b53bea01d1

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Add your code here
+
+
+//  configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         "Content-type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Buddy",
+//         dogBreed: "Fluffey Boy",
+//         dogDescription: "The bestest of pups"
+//     })
+// }
+
+
+// const submitData = (name, email) => {
+//     fetch('http://localhost:3000/users', {
+//         method: 'POST',
+//         headers: {
+//         "Content-type": "application/json", 
+//         "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: name,
+//             email: email,
+//         })
+//         .then(res => res.json())
+//         .then(data => data.id)
+//     } 
+// )};
+
+const submitData = (userName, userEmail) => {
+    
+    const newUser ={
+    name: userName,
+    email: userEmail,
+    }
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+        "Content-type": "application/json", 
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(newUser)
+
+    })
+    .then(res => res.json())
+    .then(data => addUser(data))
+    .catch(error => handleError(error))
+}
+
+function addUser (newUser) {
+    let newId = document.createElement('p');
+    newId.textContent = newUser.id
+    document.body.append(newId)
+}
+
+function handleError(error){
+    let newError = document.createElement('p')
+    newError.textContent = error.message
+    document.body.append(newError)
+}
